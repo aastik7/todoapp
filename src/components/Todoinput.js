1614f7eba192ef917c2fffec8d07356b0afe3bb4
Aastik <aastikdude7@gmail.com>
2024-07-08 23:42:42 +0530

    Task management

---
+++ src/components/Todoinput.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function Todoinput(props) {
+  const [inputText, setInputText] = useState("");
+  return (
+    <div className="input-container">
+      <input
+        type="text"
+        className="input-box-todo"
+        placeholder="Enter your task"
+        onChange={(e) => {
+          setInputText(e.target.value);
+        }}
+      ></input>
+      <button
+        className="add-btn"
+        onClick={() => {
+          props.addList(inputText);
+          setInputText("");
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+export default Todoinput;
