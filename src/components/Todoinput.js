05495d4e903272e31c7697fdc24032d9861f80f2
Aastik <aastikdude7@gmail.com>
2024-07-09 21:15:52 +0530

    adding delete button

--- src/components/Todoinput.js
+++ src/components/Todoinput.js
@@ -2,15 +2,23 @@ import React, { useState } from "react";
 
 function Todoinput(props) {
   const [inputText, setInputText] = useState("");
+  const handleEnterPress = (e) => {
+    if (e.keyCode === 13) {
+      props.addList(inputText);
+      setInputText("");
+    }
+  };
   return (
     <div className="input-container">
       <input
         type="text"
         className="input-box-todo"
         placeholder="Enter your task"
+        value={inputText}
         onChange={(e) => {
           setInputText(e.target.value);
         }}
+        onKeyDown={handleEnterPress}
       ></input>
       <button
         className="add-btn"
